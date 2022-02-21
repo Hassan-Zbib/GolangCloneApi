b7d6f2a19d0dd99f174c33f14613fba40d824dd0
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-02-21 06:33:08 +0200

    added get feed

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -22,6 +22,18 @@ type friends struct {
 	AddedAt   string      `json:"added_at"`
 }
 
+type feed struct {
+	ID         int    `json:"id"`
+	UserID     int    `json:"user_id"`
+	Content    string `json:"content"`
+	LikesCount int    `json:"likes_count"`
+	CreatedAt  string `json:"created_at"`
+	UpdatedAt  string `json:"updated_at"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	IsLiked    bool   `json:"is_liked"`
+}
+
 type request struct {
 	UserID int `json:"user_id"`
 	RecordID int `json:"record_id"`
@@ -116,7 +128,11 @@ func Friendlist(w http.ResponseWriter, r *http.Request) {
 
 	var list []friends
 
-	result, err := db.Query("SELECT u.*, f.id as request_id , f.request, f.created_at as added_at FROM users u INNER JOIN friends f  ON  f.user_id = u.id OR f.friend_id = u.id  WHERE u.id != ? AND f.request = ?", user_id, request)
+	result, err := db.Query(`SELECT u.*, f.id as request_id , f.request, f.created_at as added_at 
+								FROM users u 
+								INNER JOIN friends f  
+								ON  f.user_id = u.id OR f.friend_id = u.id  
+								WHERE u.id != ? AND f.request = ?`, user_id, request)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -180,4 +196,55 @@ func Post(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetFeed(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// db connect
+	db := config.DbConn()
+	// get json request
+	decoder := json.NewDecoder(r.Body)
+
+	var vars request
+	err := decoder.Decode(&vars)
+	if err != nil {
+        panic(err)
+    }
+
+	user_id := vars.UserID
+	request := "accepted"
+
+	var list []feed
+
+	result, err := db.Query(`SELECT s.*, u.name, u.email,  CASE 
+								WHEN ? in (SELECT DISTINCT user_id FROM likes WHERE status_id = s.id) THEN 1
+								ELSE 0
+							END as is_liked
+							FROM statuses s
+							INNER JOIN users u ON s.user_id = u.id 
+							INNER JOIN friends f ON s.user_id = f.user_id OR s.user_id = f.friend_id
+							WHERE s.user_id != ? AND f.request = ?
+							;`, user_id, user_id, request)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	for result.Next() {
+		var record feed
+
+        s := reflect.ValueOf(&record).Elem()
+        numCols := s.NumField()
+        columns := make([]interface{}, numCols)
+        for i := 0; i < numCols; i++ {
+            field := s.Field(i)
+            columns[i] = field.Addr().Interface()
+        }
+
+		err = result.Scan(columns...)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		list = append(list, record)
+	}
+	json.NewEncoder(w).Encode(list)
+	defer db.Close()
 }
